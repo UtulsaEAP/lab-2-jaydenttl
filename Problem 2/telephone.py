622b5444c93b169b70bd67867102700c3cf9cb58
Jayden Ly <jtl5627@utulsa.edu>
2024-02-01 14:45:04 -0600

    1 and 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,13 @@
 def telephone():
     phone_number = int(input())
-    ''' Type your code here. '''
+
+    area_code = (phone_number // 10000000)
+    first_three = ((phone_number // 10000) %1000)
+    last_four = (phone_number %10000)
+
+
+    print (('('+str(area_code)+') '+ str(first_three)+'-'+str(last_four)))
+
+   # print (str(area_code))
 if __name__ == "__main__":
     telephone()
