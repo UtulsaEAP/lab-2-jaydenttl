5ca0cb74d7699e5fb9cea66b0a170efe40e89327
Jayden Ly <jtl5627@utulsa.edu>
2024-02-01 14:54:03 -0600

    1 2 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -2,6 +2,13 @@
 def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
-    
+
+    six_hours = float(caffeine_mg / 2)
+    twelve_hours = float(six_hours / 2)
+    twentyfour_hours = float(twelve_hours / 4)
+
+    print (str('After 6 hours: '+f'{six_hours:.2f}'+' mg'))
+    print (str('After 12 hours: '+f'{twelve_hours:.2f}'+' mg'))
+    print (str('After 24 hours: '+f'{twentyfour_hours:.2f}'+' mg'))
 if __name__ == "__main__":
     caffeine()
