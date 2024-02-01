4f659ef70f669538d713dafdf0310c304a4730e3
Jayden Ly <jtl5627@utulsa.edu>
2024-02-01 16:08:21 -0600

    5

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -4,6 +4,17 @@ def simple_stats():
     num2 = float(input())
     num3 = float(input())
     num4 = float(input())
-    ''' Type your code here. '''
+    
+    prod = float(num1 * num2 * num3 * num4)
+    avg = float((num1 + num2 + num3 + num4)/4)
+
+    #fprod = float(num1 * num2 * num3 * num4)
+    #favg = float((num1 * num2 * num3 * num4)/4)
+
+    print(f'{prod:.0f}' , f'{avg:.0f}')
+    print(f'{prod:.3f}' , f'{avg:.3f}')
+
+    #print( int prod int avg)
+    #print( float prod float avg)
 if __name__ == "__main__":
     simple_stats()
