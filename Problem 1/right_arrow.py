dea6d262b8da3d8294a888226c9a4704589d59ec
Jayden Ly <jtl5627@utulsa.edu>
2024-02-01 15:13:16 -0600

    1234

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -2,7 +2,7 @@ def right_arrow():
     base_char = input()
     head_char = input()
 
-    row1 = base_char*6 + head_char
+    row1 = '      ' + head_char
     row2 = base_char*6 + head_char*2
     row3 = base_char*6 + head_char*3
 
--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -3,6 +3,11 @@ def real_estate():
     
     current_price = int(input())
     last_month_price = int(input())
-    # Your code goes here
+    
+    difference = int(current_price-last_month_price)
+    mortgage = float((current_price * 0.051) / 12)
+
+    print('This house is $' + str(current_price) + '. The change is $' + str(difference) + ' since last month.')
+    print('The estimated monthly mortgage is $' + (f'{mortgage:.2f}') + '.')
 if __name__ == "__main__":
     real_estate()
